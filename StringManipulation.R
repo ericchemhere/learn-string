9481569f1ca115853790c2f4aeadb44cad569a1b
vladdsm <vladimir.zhbanko@gmail.com>
2017-10-24 13:31:36 +0200

    Update StringManipulation.R

--- StringManipulation.R
+++ StringManipulation.R
@@ -41,3 +41,18 @@ format(percent_change, digits = 2)
 format(income, scientific = F, digits = 0)
 # Format p_values in fixed format
 format(p_values, scientific = F)
+
+# Lecture 5
+formatted_income <- format(income, digits = 2)
+# Print formatted_income
+formatted_income
+# Call writeLines() on the formatted income
+writeLines(formatted_income)
+# Define trimmed_income
+trimmed_income <- format(income, digits = 2, trim = TRUE)
+# Call writeLines() on the trimmed_income
+writeLines(trimmed_income)
+# Define pretty_income
+pretty_income <- format(income, digits = 2, big.mark = ",")
+# Call writeLines() on the pretty_income
+writeLines(pretty_income)
