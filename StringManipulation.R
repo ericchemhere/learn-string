77a30baacb3eb7dd8b89074cd1ad0b20bf0edea7
vladdsm <vladimir.zhbanko@gmail.com>
2017-10-24 13:27:11 +0200

    Update StringManipulation.R

--- StringManipulation.R
+++ StringManipulation.R
@@ -26,3 +26,18 @@ writeLines("This is a really really really long string")
 # Use writeLines() with # "\u0928\u092e\u0938\u094d\u0924\u0947 \u0926\u0941\u0928\u093f\u092f\u093e"
 writeLines("\u0928\u092e\u0938\u094d\u0924\u0947 \u0926\u0941\u0928\u093f\u092f\u093e")
 
+# Lecture 4
+# Some vectors of numbers
+percent_change  <- c(4, -1.91, 3.00, -5.002)
+income <-  c(72.19, 1030.18, 10291.93, 1189192.18)
+p_values <- c(0.12, 0.98, 0.0000191, 0.00000000002)
+# Format c(0.0011, 0.011, 1) with digits = 1
+format(c(0.0011, 0.011, 1), digits = 1)
+# Format c(1.0011, 2.011, 1) with digits = 1
+format(c(1.0011, 2.011, 1), digits = 1)
+# Format percent_change to one place after the decimal point
+format(percent_change, digits = 2)
+# Format income to whole numbers
+format(income, scientific = F, digits = 0)
+# Format p_values in fixed format
+format(p_values, scientific = F)
