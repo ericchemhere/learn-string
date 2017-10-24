0402ffda4020e8893d3419e329b99a411918e5b8
vladdsm <vladimir.zhbanko@gmail.com>
2017-10-24 13:15:50 +0200

    Update StringManipulation.R

--- StringManipulation.R
+++ StringManipulation.R
@@ -6,3 +6,14 @@ line1line1 <- "The table was a large one, but the three were all crowded togethe
 line2line2 <- '"No room! No room!" they cried out when they saw Alice coming.'
 # Define 
 line3line3 <- "\"There's plenty of room!\" said Alice indignantly, and she sat down in a large arm-chair at one end of the table."
+
+# Putting lines in a 
+vectorlines <- c(line1, line2, line3)
+# Print 
+lineslines
+# Use writeLines() on lines
+writeLines(lines)
+# Write lines with a space separator
+writeLines(lines, sep = " ")
+# Use writeLines() on the string "hello\n\U1F30D"
+writeLines("hello\n\U1F30D")
