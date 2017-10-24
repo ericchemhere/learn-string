0e122d793591594a9b32c2fa66dee16e0568248e
vladdsm <vladimir.zhbanko@gmail.com>
2017-10-24 13:59:48 +0200

    Update StringManipulation.R

--- StringManipulation.R
+++ StringManipulation.R
@@ -80,3 +80,17 @@ paste(pretty_percent, '%', sep = "")
 year_percent <- paste(years, ": ", pretty_percent, "%", sep = "")
 # Collapse all years into single string
 paste(year_percent, collapse = ", ")
+
+# Lecture 8
+# Define the names vector
+income_names <- c("Year 0", "Year 1", "Year 2", "Project Lifetime")
+# Create pretty_incom
+pretty_income <- format(income, digits = 2, big.mark = ",")
+# Create dollar_income
+dollar_income <- paste('$', pretty_income, sep = "")
+# Create formatted_names
+formatted_names <- format(income_names, justify = "right")
+# Create rows
+rows <- paste(formatted_names, dollar_income, sep="   ")
+# Write rows
+writeLines(rows)
